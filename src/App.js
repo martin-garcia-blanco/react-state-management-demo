f859bdaffb1be4ff0495917937aff97827841913
martin-garcia-blanco <programadopormartin@gmail.com>
2022-02-02 22:39:56 +0100

    ✌🏼✌🏼✌🏼 Fetch pokemons and store them in memory

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import PokemonCard from './components/PokemonCard/PokemonCard';
+import { getPokemons } from './services/pokemon';
 
 
 function App() {
-  const pokemons = []
-  const toggleFavouritePokemon = () => {}
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(()=>{
+    getPokemons()
+    .then(setPokemons)
+  }, [])
 
   return (
     <div className="App">
       {
-        pokemons.map((pokemon, index) => <PokemonCard key={index} pokemon={pokemon} toggleFavourite={toggleFavouritePokemon}/> )
+        pokemons.map((pokemon, index) => <PokemonCard key={index} pokemon={pokemon} toggleFavourite={()=>{}}/> )
       }
     </div>
   );
