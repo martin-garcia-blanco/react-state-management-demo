e7948edf4d7ab87fc13e61da9c57a187bf5e92f5
martin-garcia-blanco <programadopormartin@gmail.com>
2022-02-03 11:56:53 +0100

    Create a mock of pokemons array to skip asyn stuff in redux

---
+++ src/services/pokemonsMock.js
@@ -0,0 +1,54 @@
+const pokemonsMock = [
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    isFavourite: false,
+    name: "bulbasaur"
+  },
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+    isFavourite: false,
+    name: "ivysaur"
+  },
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+    isFavourite: false,
+    name: "venusaur"
+  },
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    isFavourite: false,
+    name: "charmander"
+  },
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+    isFavourite: false,
+    name: "charmeleon"
+  },
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+    isFavourite: false,
+    name: "charizard"
+  },
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+    isFavourite: false,
+    name: "squirtle"
+  },
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",
+    isFavourite: false,
+    name: "wartortle"  
+  },
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+    isFavourite: false,
+    name: "blastoise"
+  },
+  {
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png",
+    isFavourite: false,
+    name: "caterpie"
+  },
+]
+
+export default pokemonsMock
